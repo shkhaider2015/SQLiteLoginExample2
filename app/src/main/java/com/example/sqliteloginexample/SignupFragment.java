0578ac7c84b1b534bb10e3e65c4774668a10ff74
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-08-18 14:50:22 +0500

    Select Image Method Complete

--- app/src/main/java/com/example/sqliteloginexample/SignupFragment.java
+++ app/src/main/java/com/example/sqliteloginexample/SignupFragment.java
@@ -1,10 +1,13 @@
 package com.example.sqliteloginexample;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -12,7 +15,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SignupFragment extends Fragment {
+public class SignupFragment extends Fragment implements View.OnClickListener {
 
     private EditText
             mFullName,
@@ -21,12 +24,16 @@ public class SignupFragment extends Fragment {
             mConfirmPassword,
             mCellNumber;
     private ImageButton mProfilePicture;
+    private Button mSignup;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.signup, container, false);
         init(view);
+
+        mProfilePicture.setOnClickListener(this);
+        mSignup.setOnClickListener(this);
 
         return view;
     }
@@ -39,8 +46,42 @@ public class SignupFragment extends Fragment {
         mConfirmPassword = view.findViewById(R.id.signup_confirm_password);
         mCellNumber = view.findViewById(R.id.signup_phone);
         mProfilePicture = view.findViewById(R.id.signup_pic);
+        mSignup = view.findViewById(R.id.signup_signup);
     }
 
+    private void selectImage()
+    {
+        final CharSequence[] items = { "Take Photo", "Choose from Library", "Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Add Photo");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                boolean result = Utility.checkPermission(getActivity());
+
+                if(items[i].equals("Take Photo"))
+                {
+                    userChoosenTask = "Take Photo";
+                    if(result)
+                        cameraIntent();
+                }
+                else if(items[i].equals("Choose From Library"))
+                {
+                    userChoosenTask = "Choose From Library";
+                    if(result)
+                        galleryIntent();
+                }
+                else if(items[i].equals("Cancel"))
+                {
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+
+        builder.show();
+    }
     private void validateInfo()
     {
         String name, email, password, cPassword, cellNumber;
@@ -105,5 +146,16 @@ public class SignupFragment extends Fragment {
     private void loadToDatabase(String name, String email, String password, String cellNumber)
     {
         
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.signup_pic:
+                break;
+            case R.id.signup_signup:
+                break;
+        }
     }
 }
