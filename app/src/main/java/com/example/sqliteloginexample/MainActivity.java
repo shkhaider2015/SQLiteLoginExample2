567f5d44f8c360d90b8a7d4dce66cc439920c038
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-08-25 13:45:52 +0500

    Home Progress bar ....

--- app/src/main/java/com/example/sqliteloginexample/MainActivity.java
+++ app/src/main/java/com/example/sqliteloginexample/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.sqliteloginexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProgressBar mProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +18,28 @@ public class MainActivity extends AppCompatActivity {
     private void init()
     {
 
-        
+        mProgressBar = findViewById(R.id.main_progress);
+    }
+
+    private void progressTime()
+    {
+
+        CountDownTimer countDownTimer = new CountDownTimer(50000, 500) {
+            int total = 0;
+            @Override
+            public void onTick(long var)
+            {
+                total += var;
+                mProgressBar.setProgress(total);
+
+            }
+
+            @Override
+            public void onFinish()
+            {
+                //do something
+
+            }
+        }.start();
     }
 }
