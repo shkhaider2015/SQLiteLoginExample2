00b4d32be2064dc708aecc2ce92da9c74f0aa85a
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-08-30 10:35:45 +0500

    count down timer ..

--- app/src/main/java/com/example/sqliteloginexample/LoginFragment.java
+++ app/src/main/java/com/example/sqliteloginexample/LoginFragment.java
@@ -51,7 +51,6 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
             case R.id.login_login:
                 //commit 1
                 //commit 2
-                //commit 3
                 break;
             case R.id.login_signup:
                 break;
